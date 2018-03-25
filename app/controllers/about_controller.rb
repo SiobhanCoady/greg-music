b6799df415b397aceae9dcb86907ba1bef44d23b
Siobhan Coady <siobhan.coady@gmail.com>
2018-03-25 11:04:57 -0700

    Add welcome, about Greg, and about the space views and add bootstrap

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,9 @@
+class AboutController < ApplicationController
+    def greg
+        render :greg
+    end
+
+    def space
+        render :space
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get('/about/greg', { to: 'about#greg', as: 'about_greg' })
+  get('/about/space', { to: 'about#space', as: 'about_space' })
+
+  root 'welcome#index'
 end
