7ae914a56a089653c73009c20ac0da963b30f87f
Siobhan Coady <siobhan.coady@gmail.com>
2018-03-25 17:55:21 -0700

    Add navbar and other controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get('/about/greg', { to: 'about#greg', as: 'about_greg' })
   get('/about/space', { to: 'about#space', as: 'about_space' })
+  get('/music', { to: 'music#index', as: 'music_index' })
+  get('/events', { to: 'events#index', as: 'events_index' })
+  get('/contact', { to: 'contact#index', as: 'contact_index' })
 
   root 'welcome#index'
 end
